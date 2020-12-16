823f935fdbd1f431c3416af00a0e3af898dec59d
JJKOrlino <jjkorlino80@yahoo.com>
2020-12-15 18:00:48 -0600

    commit tree 5078ad02451be524ec2a01022d7bc2fba7deb4f4

---
+++ lib/insert.sql
@@ -0,0 +1,85 @@
+-- INSERT INTO series VALUES (1, "Naruto", "1", "Anime");
+-- INSERT INTO series VALUES (2, "Dragon Ball", "2", "Anime");
+
+-- INSERT INTO books VALUES (1, "Naruto One", 2002, "1");
+-- INSERT INTO books VALUES (2, "Naruto Two", 2002, "1");
+-- INSERT INTO books VALUES (3, "Naruto Three", 2003, "1");
+-- INSERT INTO books VALUES (4, "Naruto Four", 2003, "1");
+-- INSERT INTO books VALUES (5, "Dragon Ball 1", 1986, "2");
+-- INSERT INTO books VALUES (6, "Dragon Ball 2", 1986, "2");
+
+-- INSERT INTO characters VALUES (1, "Naruto", "Sasuke lame", "Jinchuriki", "1");
+-- INSERT INTO characters VALUES (2, "Sasuke", "I hate my brother", "Human", "1");
+-- INSERT INTO characters VALUES (3, "Sakura", "Sasuke so cool", "Human", "1");
+-- INSERT INTO characters VALUES (4, "Kakashi", "Three brats", "Human", "1");
+-- INSERT INTO characters VALUES (5, "Goku", "", "Yell for power", "Saiyan" "2");
+-- INSERT INTO characters VALUES (6, "Piccolo", "Finger laser beam", "Namekian", "2");
+-- INSERT INTO characters VALUES (7, "Vegeta", "I wanna be strong", "Saiyan", "2");
+-- INSERT INTO characters VALUES (8, "Frieza", "Stupid monkey", "Changeling", "2");
+
+-- INSERT INTO subgenres VALUES (1, "Cartoon");
+-- INSERT INTO subgenres VALUES (2, "Action");
+
+-- INSERT INTO authors VALUES (1, "Kishimoto");
+-- INSERT INTO authors VALUES (2, "Toriyama");
+
+-- INSERT INTO character_books VALUES (1, "1", "2");
+-- INSERT INTO character_books VALUES (2, "1", "2");
+-- INSERT INTO character_books VALUES (3, "2", "1");
+-- INSERT INTO character_books VALUES (4, "2", "1");
+-- INSERT INTO character_books VALUES (5, "1", "1");
+-- INSERT INTO character_books VALUES (6, "2", "2");
+-- INSERT INTO character_books VALUES (7, "2", "2");
+-- INSERT INTO character_books VALUES (8, "2", "1");
+-- INSERT INTO character_books VALUES (9, "1", "1");
+-- INSERT INTO character_books VALUES (10, "2", "1");
+-- INSERT INTO character_books VALUES (11, "1", "1");
+-- INSERT INTO character_books VALUES (12, "2", "1");
+-- INSERT INTO character_books VALUES (13, "1", "2");
+-- INSERT INTO character_books VALUES (14, "1", "1");
+-- INSERT INTO character_books VALUES (15, "2", "2");
+-- INSERT INTO character_books VALUES (16, "2", "1");
+
+INSERT INTO series VALUES (1, "Harry Potter", "1", "magic");
+INSERT INTO series VALUES (2, "Lord of the Rings", "2", "fantasy"); 
+
+
+INSERT INTO books VALUES (1, "Harry Potter one", 2000, "1");
+INSERT INTO books VALUES (2, "Harry Potter two", 2001, "1");
+INSERT INTO books VALUES (3, "Harry Potter three", 2002, "1");
+INSERT INTO books VALUES (4, "Harry Potter four", 2003, "1");
+INSERT INTO books VALUES (5, "Hobbit", 1970, "2");
+INSERT INTO books VALUES (6, "Lord of the Rings one", 1980, "2");
+
+INSERT INTO characters VALUES (1, "Harry", "my scar hurts", "human", "1");
+INSERT INTO characters VALUES (2, "Ron", "my rat", "human", "1");
+INSERT INTO characters VALUES (3, "Hermione", "I'm so smart", "human", "1");
+INSERT INTO characters VALUES (4, "Froto", "keep the ring safe", "mini human", "2");
+INSERT INTO characters VALUES (5, "Samis", "evil!", "magic human", "2");
+INSERT INTO characters VALUES (6, "Galum", "the prescious", "evil thing", "2");
+INSERT INTO characters VALUES (7, "Orcs", "work work work", "evil thing", "2");
+INSERT INTO characters VALUES (8, "Elves", "arrows", "magic things", "2");
+
+
+INSERT INTO subgenres VALUES (1, "Magic");
+INSERT INTO subgenres VALUES (2, "Fantisy");
+
+INSERT INTO authors VALUES (1, "J.K Rowling");
+INSERT INTO authors VALUES (2, "Tolkin");
+
+INSERT INTO character_books VALUES (1, "1", "2");
+INSERT INTO character_books VALUES (2, "1", "2");
+INSERT INTO character_books VALUES (3, "2", "1");
+INSERT INTO character_books VALUES (4, "2", "1");
+INSERT INTO character_books VALUES (5, "1", "1");
+INSERT INTO character_books VALUES (6, "2", "2");
+INSERT INTO character_books VALUES (7, "2", "2");
+INSERT INTO character_books VALUES (8, "2", "1");
+INSERT INTO character_books VALUES (9, "1", "1");
+INSERT INTO character_books VALUES (10, "2", "1");
+INSERT INTO character_books VALUES (11, "1", "1");
+INSERT INTO character_books VALUES (12, "2", "1");
+INSERT INTO character_books VALUES (13, "1", "2");
+INSERT INTO character_books VALUES (14, "1", "1");
+INSERT INTO character_books VALUES (15, "2", "2");
+INSERT INTO character_books VALUES (16, "2", "1");
